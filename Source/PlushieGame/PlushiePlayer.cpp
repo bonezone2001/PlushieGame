e459b381b777f7277b8cc315b0242bda6bfe339a
Kyle Pelham <bonezone2001@gmail.com>
2022-06-22 22:58:26 +0100

    initial project

---
+++ Source/PlushieGame/PlushiePlayer.cpp
@@ -0,0 +1,77 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlushiePlayer.h"
+#include "Components/StaticMeshComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+APlushiePlayer::APlushiePlayer()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create mesh
+	playerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	SetRootComponent(playerMesh);
+
+	// Create boom arm
+	springArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	springArm->SetupAttachment(RootComponent);
+	springArm->bUsePawnControlRotation = true;
+
+	// Create camera
+	camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	camera->SetupAttachment(springArm, USpringArmComponent::SocketName);
+}
+
+// Called when the game starts or when spawned
+void APlushiePlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APlushiePlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+
+}
+
+// Called to bind functionality to input
+void APlushiePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Bind camera events
+	PlayerInputComponent->BindAxis("TurnYaw", this, &APlushiePlayer::TurnYaw);
+	PlayerInputComponent->BindAxis("TurnPitch", this, &APlushiePlayer::TurnPitch);
+	PlayerInputComponent->BindAxis("Zoom", this, &APlushiePlayer::Zoom);
+
+	// Bind power
+	PlayerInputComponent->BindAction("PlushiePower", IE_Pressed, this, &APlushiePlayer::TogglePower);
+}
+
+void APlushiePlayer::TurnPitch(float value)
+{
+	AddControllerPitchInput(value);
+}
+
+void APlushiePlayer::TurnYaw(float value)
+{
+	AddControllerYawInput(value);
+}
+
+void APlushiePlayer::Zoom(float value)
+{
+	UE_LOG(LogTemp, Warning, TEXT("Zoom: %f"), value);
+}
+
+void APlushiePlayer::TogglePower()
+{
+
+}
+
---
+++ Source/PlushieGame/PlushiePlayer.h
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "PlushiePlayer.generated.h"
+
+UCLASS()
+class PLUSHIEGAME_API APlushiePlayer : public APawn
+{
+	GENERATED_BODY()
+
+	// Player mesh
+	UPROPERTY(EditAnywhere, Category = "Player")
+	class UStaticMeshComponent* playerMesh;
+
+	// Player boom arm
+	UPROPERTY(VisibleDefaultsOnly, Category = "Player")
+	class USpringArmComponent* springArm;
+
+	// Player camera
+	UPROPERTY(VisibleDefaultsOnly, Category = "Player")
+	class UCameraComponent* camera;
+
+public:
+	// Sets default values for this pawn's properties
+	APlushiePlayer();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Turn pitch
+	virtual void TurnPitch(float value);
+
+	// Turn yaw
+	virtual void TurnYaw(float value);
+
+	// Zoom
+	virtual void Zoom(float value);
+
+	// Power
+	virtual void TogglePower();
+};
